65665047a86b16c665b2902e59b92359067b0ac5
boyunjjang1 <boyunqueen@gmail.com>
2018-06-10 18:19:43 +0900

    UPDATE boyomiCloset.py

--- BoyomiCloset/boyomiCloset.py
+++ BoyomiCloset/boyomiCloset.py
@@ -12,8 +12,33 @@ def index():
     login_error = None
     pageNum = None
     pageNumList = None
+
+
+    pageNum = request.args.get('page')
+    if pageNum is None:
+        pageNum = '1'
+    pageNum = int(pageNum)
+    if pageNum < 1:
+        pageNum = 1
+    app.logger.debug('index() - pageNum : %d' % pageNum)
+
+
+    keyword = None
+    keyword = request.args.get('keyword')
+    app.logger.debug('index() - keyword : %s' % keyword)
+
+
+    startNum, endNum = 0, 0
+    if pageNum >= 3:
+        startNum = pageNum - 2
+    else:
+        startNum = 1
+    endNum = startNum + 4
+    pageNumList = list(range(startNum, endNum+1))
+    app.logger.debug('index() - pageNumList : %s' % pageNumList)
+
     if isLogin():
-        return render_template('index.html', u_name = session['u_name'], login_error = login_error)
+        return render_template('index.html', u_name = session['u_name'], login_error = login_error, pageNum = pageNum, pageNumList = pageNumList, keyword = keyword)
     else:
         if request.method == 'POST':
             id = request.form["id"]
@@ -34,9 +59,9 @@ def index():
                 return redirect(url_for('index'))
             else:
                 login_error = '로그인에 실패하셨습니다.'
-                return render_template('index.html', u_name = u_name, login_error = login_error)
+                return render_template('index.html', u_name = u_name, login_error = login_error, pageNum = pageNum, pageNumList = pageNumList, keyword = keyword)
         else:
-            return render_template('index.html', u_name = u_name, login_error = login_error)
+            return render_template('index.html', u_name = u_name, login_error = login_error, pageNum = pageNum, pageNumList = pageNumList, keyword = keyword)
 
 # 회원기능 - 회원등록
 @app.route('/user/join', methods=['GET','POST'])
@@ -167,6 +192,178 @@ def user_leave():
     
     else:
         return render_template('user_notlogin.html')
+
+
+# 게시판 기능 - 게시판_글목록
+@app.route('/board/')
+def board_list():
+    # 조회 할 페이지 번호를 가져옴 get query string
+    pageNum = request.args.get('page')
+    if pageNum is None:
+        pageNum = '1'
+    pageNum = int(pageNum)
+    if pageNum < 1:
+        pageNum = 1
+    app.logger.debug('board_list() - pageNum : %d' % pageNum)
+
+
+    keyword = None
+    keyword = request.args.get('keyword')
+    app.logger.debug('board_list() - keyword : %s' % keyword)
+
+
+    # DB로 부터 tbboard 게시판 글 목록을 가져옴
+    offset = (pageNum - 1) * 10
+    params = (offset,)
+    con = sqlite3.connect('mywebsite_tables')
+    cur = con.cursor()
+    if keyword == None:
+        cur.execute('''
+            SELECT b_id, u_name, title, b.add_at FROM tbboard AS b
+            INNER JOIN tbuser AS u ON b.u_id = u.u_id
+            WHERE b.use_flag = 'Y' ORDER BY b_id DESC LIMIT 10 OFFSET ?;
+            ''', params)
+    else:
+        params = (keyword, keyword, offset)
+        cur.execute('''
+            SELECT b_id, u_name, title, b.add_at FROM tbboard AS b
+            INNER JOIN tbuser AS u ON b.u_id = u.u_id
+            WHERE b.use_flag = 'Y' AND (b.title like '%'||?||'%'
+            OR b.content like '%'||?||'%') ORDER BY b_id DESC LIMIT 10 OFFSET ?;
+        ''', params)
+        rows = cur.fetchall()
+        con.close()
+    
+    return render_template('board_list.html', board_rows = rows)
+
+
+
+# 게시판 기능 - 게시판_글 등록
+@app.route('/board/post', methods = ['GET','POST'])
+def board_post():
+    if isLogin():
+        if request.method == 'POST':
+            u_id = session['u_id']
+            title = request.form['title']
+            content = request.form['content']
+            params = (u_id, title, content)
+            app.logger.debug('board_post() - %s' % str(params))
+
+            con = sqlite3.connect('mywebsite_creatables')
+            cur = con.cursor()
+            cur.execute('''
+                INSERT INTO tbboard (u_id, title, content) VALUES (?, ?, ?);
+            ''', params)
+
+            con.commit()
+            con.close()
+            return redirect(url_for('index'))
+        else:
+            u_name = session['u_id']
+            return render_template('board_post.html', u_name = u_name)
+    else:
+        return render_template('user_notlogin.html')
+
+# 게시판 기능 - 게시판_글 수정
+@app.route('/board/<int:board_id>/update', methods=['GET','POST'])
+def board_update(board_id):
+    if isLogin():
+        if request.method == 'GET':
+            u_id = session['u_id']
+            params = (board_id,)
+            app.logger.debug('board_update() - %s' % str(params))
+            con = sqlite3.connect('mywebsite_createtables')
+            cur = con.cursor()
+            cur.execute('''
+                SELECT b_id, b.u_id, u_name, title, content, b.add_at, b.upd_at
+                FROM tbboard AS b INNER JOIN tbuser AS u
+                ON b.u_id = u.u_id
+                WHERE b.use_flag = 'Y' AND b.u_id = ?;
+            ''',params)
+            rows = cur.fetchall()            
+            app.logger.debug('board_update() - %s' %str(params))
+            con.close()
+            
+            board_view = rows[0]
+            if len(rows) == 1:
+                u_name = session['u_name']
+                if u_id == board_view[1]:
+                    return render_template('board_update.html',board_view=board_view, u_id = u_id, u_name = u_name)
+                else:
+                    return render_template('user_notlogin.html')
+            else:
+                return render_template('user_notlogin.html')
+        
+        else:
+            u_id = session['u_id']
+            b_id = int(request.form['b_id'])
+            title = request.form['title']
+            content = request.form['content']
+            params = (title, content, b_id, u_id)
+            app.logger.debug('board_update() - params: %s' % str(params))
+
+            if b_id != board_id:
+                return render_template('user_notlogin.html')
+            
+            con = sqlite3.connect('mywebsite_createtables')
+            cur = con.cursor()
+            cur.execute('''
+                UPDATE tbboard SET title=?, content=?, upd_at=DATETIME('now', 'localtime')
+                WHERE b_id=? AND u_id=?;
+            ''', params)
+            con.commit()
+            con.close()
+            return redirect(url_for('board_view', board_id = board_id))
+    else:
+        return render_template('user_notlogin.html')
+
+# 게시판 기능 - 글_삭제
+@app.route('/board/<int:b_id>/delete')
+def board_delete(board_id):
+    if isLogin():
+        u_id = session['u_id']
+        params = (board_id, u_id)
+        app.logger.debug('board_delete() - %s' %str(params))
+        
+        con = sqlite3.connect('mywebsite_createtables')
+        cur = con.cursor()
+        cur.execute('''
+            UPDATE tbboard SET use_flag = 'N', upd_dt=DATETIME('now', 'localtime')
+            WHERE b_id=? AND u_id=?
+        ''', params)
+        con.commit()
+        con.close()
+        return redirect(url_for('index'))
+    else:
+        return render_template('user_notlogin.html')
+
+# 게시판 기능 - 게시판_글보기
+@app.route('/board/<int:board_id>')
+def board_view(board_id):
+    if isLogin(): #'u_id' in session:   # 로그인 되어 있다면
+        u_id = session['u_id']
+        params = (board_id,)
+        app.logger.debug('board_view() - %s' % str(params))
+        con=sqlite3.connect('mywebsite')
+        cur=con.cursor()
+        cur.execute('''
+        SELECT b_id, b.u_id, u_name, title, content, b.add_dt, b.upd_dt
+        FROM tbboard b INNER JOIN tbuser u
+        ON b.u_id = u.u_id
+        WHERE b.use_flag='Y' AND b_id=?;
+            ''', params)
+        rows=cur.fetchall()
+        app.logger.debug('board_view() - %s' % str(rows))
+        con.close()
+        board_view = rows[0]
+        if len(rows) == 1:  # 로그인 정상
+            u_name=session['u_name']
+            return render_template('board_view.html', board_view=board_view, u_id=u_id, u_name=u_name)
+        else:
+            return render_template('user_notlogin.html')   # 잘못된 페이지 접근이라고 보여주고 이동해도 될듯
+    else:                               # 로그인 되어 있지 않다면
+        return render_template('user_notlogin.html')   # 잘못된 페이지 접근이라고 보여주고 이동해도 될듯
+    
 
 # admin기능 - 파일 업로드
 
