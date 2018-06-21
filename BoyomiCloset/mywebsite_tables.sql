01e585ad8d4a8c1b7d11597897332b4b3b091fd4
boyunjjang1 <boyunqueen@gmail.com>
2018-06-22 02:32:15 +0900

    UPDATE mywebsite_tables.sql

--- BoyomiCloset/mywebsite_tables.sql
+++ BoyomiCloset/mywebsite_tables.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS tbuser;
 DROP TABLE IF EXISTS tbboard;
-DROP TABLE IF EXISTS tbproduct;
+DROP TABLE IF EXISTS comment;
 
 CREATE TABLE IF NOT EXISTS tbuser(
     u_id INTEGER PRIMARY KEY AUTOINCREMENT,
     input_id TEXT NOT NULL UNIQUE,
-    email TEXT NULL UNIQUE,
+    email TEXT NULL,
     pw TEXT NOT NULL,
     u_name TEXT NOT NULL,
     gender TEXT NULL,
@@ -19,15 +19,16 @@ CREATE TABLE IF NOT EXISTS tbuser(
 );
 
 
+
 -- INSERT INTO tbuser (input_id, pw, u_name) VALUES ('godboyun', 'boyunjjang1', 'Admin');
 
 CREATE TABLE IF NOT EXISTS tbproduct(
     p_id INTEGER PRIMARY KEY AUTOINCREMENT,
     p_name TEXT NOT NULL UNIQUE,
-    price INTEGER NOT NULL,
-    img_path TEXT NOT NULL UNIQUE
+    price INTEGER DEFAULT 0,
+    img_path TEXT NOT NULL UNIQUE,
+    kind TEXT NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS tbboard(
     b_id INTEGER PRIMARY KEY AUTOINCREMENT,
     u_id INTEGER NOT NULL,
@@ -38,11 +39,21 @@ CREATE TABLE IF NOT EXISTS tbboard(
     use_flag TEXT DEFAULT('Y')
 );
 
+CREATE TABLE IF NOT EXISTS comment(
+    cmt INTEGER PRIMARY KEY AUTOINCREMENT,
+    b_id INTEGER NOT NULL,
+    u_name TEXT NOT NULL,
+    naeyong TEXT NOT NULL,
+    add_at DATETIME DEFAULT (DATETIME('now', 'localtime')),
+    upd_at DATETIME DEFAULT (DATETIME('now', 'localtime'))
+);
+
 
 -- 자료형은 바뀐다
 
-INSERT OR REPLACE INTO tbuser (input_id, email, pw, u_name) VALUES (1, 'admin@example.com', 'admin', '관리자');
-INSERT OR REPLACE INTO tbuser (input_id, email, pw, u_name) VALUES (2, 'user@example.com', 'user', '사용자');
+INSERT OR REPLACE INTO tbuser (input_id, email, pw, u_name) VALUES ('1', 'admin@example.com', 'admin', '관리자');
+INSERT OR REPLACE INTO tbuser (input_id, email, pw, u_name) VALUES ('2', 'user@example.com', 'user', '사용자');
+INSERT INTO tbuser (input_id, email, pw, u_name) VALUES ('admin', 'boyunjjang1@naver.com', 'boyun1004', '운영자');
 INSERT INTO tbboard (u_id, title, content) VALUES (1, '공지사항입니다.', '공지내용 볼 꺼 뭐 있나요.');
 INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 01_01_과목소개', '강의자료 01_01_과목소개');
 INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 01_02_HTML5', '강의자료 01_02_HTML5');
@@ -68,3 +79,4 @@ INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 12_02_Python
 INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 13_01_Python_Flask 2', '강의자료 13_01_Python_Flask 2');
 INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 14_01_Website_project', '강의자료 14_01_Website_project');
 INSERT INTO tbboard (u_id, title, content) VALUES (2, '강의자료 14_01_Website_project 2', '강의자료 14_01_Website_project 2');
+
